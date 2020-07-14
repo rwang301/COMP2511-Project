811dd04ec612bf5c4a9233a23eb9d2ff7bcceb6c
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-07-15 01:35:27 +1000

    Simply SHIFT key

--- src/unsw/dungeon/DungeonController.java
+++ src/unsw/dungeon/DungeonController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 
@@ -54,42 +53,41 @@ public class DungeonController {
 
     @FXML
     public void handleKeyPress(KeyEvent event) {
-        if (event.getCode() == KeyCode.SHIFT) {
-            this.shift = true;
-        }
         switch (event.getCode()) {
         case UP:
-            if (shift == true) {
+            if (shift) {
                 player.moveBoulder("up");
-                this.shift = false;
+                shift = false;
             } else {
                 player.moveUp();
             }
             break;
         case DOWN:
-            if (shift == true) {
+            if (shift) {
                 player.moveBoulder("down");
-                this.shift = false;
+                shift = false;
             } else {
                 player.moveDown();
             }
             break;
         case LEFT:
-            if (shift == true) {
+            if (shift) {
                 player.moveBoulder("left");
-                this.shift = false;
+                shift = false;
             } else {
                 player.moveLeft();
             }
             break;
         case RIGHT:
-            if (shift == true) {
+            if (shift) {
                 player.moveBoulder("right");
-                this.shift = false;
+                shift = false;
             } else {
                 player.moveRight();
             }
             break;
+        case SHIFT:
+            shift = true;
         default:
             break;
         }
