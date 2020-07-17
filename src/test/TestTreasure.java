930c019b6958ca85f04c90220e3b32ea31b5f37c
Verity Webb <z5113759@ad.unsw.edu.au>
2020-07-17 22:00:44 +1000

    added tests for treasure

--- src/test/TestTreasure.java
+++ src/test/TestTreasure.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import unsw.dungeon.Dungeon;
 import unsw.dungeon.Player;
-import unsw.dungeon.Wall;
 import unsw.dungeon.Treasure;
 import unsw.dungeon.Component;
 import unsw.dungeon.GoalTreasure;
@@ -13,7 +12,8 @@ import unsw.dungeon.GoalTreasure;
 public class TestTreasure {
     private Dungeon dungeon = new Dungeon(4, 4);
     private Player player = new Player(dungeon, 0, 0);
-    private Treasure treasure = new Treasure(0,1);
+    private Treasure treasure = new Treasure(0, 1);
+    private Treasure treasure2 = new Treasure(0, 2);
     private Component goalTreasure = new GoalTreasure();
 
     private void initilise() {
@@ -25,14 +25,26 @@ public class TestTreasure {
     /**
      * Given a player picked up a treasure. Then it disappears. 
      */
-  //  @Test
-    //public void testTreasurePickUp() {
-      //  player.moveDown();
-
-    //}
+    @Test
+    public void testTreasurePickUp() {
+        initilise();
+        assertEquals(dungeon.getEntities().contains(treasure), true);
+        player.moveDown();
+        assertEquals(dungeon.getEntities().contains(treasure), false);
+    }
 
     /**
      * Given a player has picked up all the treasure in a dungeon. 
      * Then the goal of collecting all treasure is completed. 
      */
+    @Test
+    public void testTreasureGoal() {
+        initilise();
+        dungeon.addEntity(treasure2);
+        assertEquals(dungeon.getComplete(), false);
+        player.moveDown();
+        assertEquals(dungeon.getComplete(), false);
+        player.moveDown();
+        assertEquals(dungeon.getComplete(), true);
+    }
 }
