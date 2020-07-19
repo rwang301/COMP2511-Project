9dae8ea234c3d1154035281d7277ea919658a49b
Verity Webb <z5113759@ad.unsw.edu.au>
2020-07-19 21:34:30 +1000

    started enemy and potion tests

--- src/test/TestEnemy.java
+++ src/test/TestEnemy.java
@@ -1,5 +1,59 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+import unsw.dungeon.Enemy;
+import unsw.dungeon.Dungeon;
+import unsw.dungeon.Player;
+
 public class TestEnemy {
+    private Dungeon dungeon = new Dungeon(4, 4);
+    private Player player = new Player(dungeon, 0, 0);
+    private Enemy enemy1 = new Enemy(dungeon, 1, 1);
+    private Enemy enemy2 = new Enemy(dungeon, 1, 2);
+    private Enemy enemy3 = new Enemy(dungeon, 1, 3);
+    private Enemy enemy4 = new Enemy(dungeon, 2, 1);
+    private Enemy enemy5 = new Enemy(dungeon, 2, 2);
+    private Enemy enemy6 = new Enemy(dungeon, 2, 3);
+
+    public void initialise() {
+        dungeon.setPlayer(player);
+        dungeon.addEntity(enemy1);
+        enemy1.initialise(player);
+    }
+
+    /**
+     * Given the player does not hold a potion. Then the enemies move toward them. 
+     */
+
+     /**
+      * Given the player holds a potion. Then the enemies move away from them. 
+      */
+
+    /**
+     * Given the player does not hold a potion. When the enemies cannot move any closer to the player. Then the enemies stop. 
+     */
+
+    /**
+     * Given a wall is on a square. When an enemy tries to move onto that square.
+     * The enemy stays where they were. 
+     */
     
+     /**
+      * Given a player does not hold a sword nor a potion. When an enemy collides with the player. Then the player dies. 
+      */
+
+
+     /**
+      * Given a floor switch has no entities on it. When an enemy tries to go over the floor switch. 
+      * Then the enemy goes through. 
+      */
+
+      /**
+       * Given an enemy goes through a portal. Then the enemy appears on the corresponding portal. 
+       */
 }
---
+++ src/test/TestPotion.java
@@ -0,0 +1,66 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+import unsw.dungeon.Component;
+import unsw.dungeon.Enemy;
+import unsw.dungeon.Dungeon;
+import unsw.dungeon.GoalEnemies;
+import unsw.dungeon.Potion;
+import unsw.dungeon.Player;
+import java.time.LocalTime;
+
+public class TestPotion {
+    private Dungeon dungeon = new Dungeon(4, 4);
+    private Player player = new Player(dungeon, 0, 0);
+    private Potion potion1 = new Potion(1, 0);
+    private Potion potion2 = new Potion(2, 0);
+    private Component goalEnemies = new GoalEnemies();
+    private Enemy enemy1 = new Enemy(dungeon, 1, 1);
+    private Enemy enemy2 = new Enemy(dungeon, 1, 2);  
+
+    public void initialise() {
+        dungeon.setPlayer(player);
+        dungeon.setGoal(goalEnemies);
+        dungeon.addEntity(potion1);
+        dungeon.addEntity(potion2);
+        dungeon.addEntity(enemy1);
+        dungeon.addEntity(enemy2);
+        enemy1.initialise(player);
+        enemy2.initialise(player);
+    }
+
+    /**
+      * Given a player has picked up a potion. When 5 seconds of time has passed. Then the potion effect disappears.
+      */
+    @Test
+    public void testCanPickup() {
+        initialise();
+        player.moveRight();
+        assertFalse(dungeon.getEntities().contains(potion1));
+        assertTrue(player.getPotion() != null);
+        LocalTime startTime = java.time.LocalTime.now();
+    }
+
+    /**
+     * Given the player does not hold a potion. Then the enemies move toward them. 
+     */
+
+     /**
+      * Given the player holds a potion. Then the enemies move away from them. 
+      */
+        
+    /**
+     * Given the player does not hold a potion. When the enemies cannot move any closer to the player. Then the enemies stop. 
+     */
+
+    /**
+     * Given a player holds a potion. When the player collides with an enemy. Then the enemy disappears. 
+     */
+
+     
+}
