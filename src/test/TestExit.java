58d6ad380daa16a6f535ff8b21f03e1688ae79cd
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-07-20 11:24:38 +1000

    Add more complex goalas

--- src/test/TestExit.java
+++ src/test/TestExit.java
@@ -12,6 +12,7 @@ import unsw.dungeon.GoalExit;
 import unsw.dungeon.GoalTreasure;
 import unsw.dungeon.Or;
 import unsw.dungeon.GoalBoulders;
+import unsw.dungeon.GoalEnemies;
 import unsw.dungeon.Switch;
 import unsw.dungeon.Treasure;
 import unsw.dungeon.Boulder;
@@ -25,14 +26,19 @@ public class TestExit {
     private Exit exit = new Exit(0, 1);
     private Switch floorSwitch = new Switch(2, 1);
     private Boulder boulder = new Boulder(1, 1);
+    private Treasure treasure = new Treasure(0, 2);
     private Component goalExit = new GoalExit();
     private Component goalBoulders = new GoalBoulders();
     private Component goalTreasure = new GoalTreasure();
+    private Component goalEnemies = new GoalEnemies();
 
     private void initialise() {
         dungeon.setPlayer(player);
         dungeon.setGoal(goalExit);
         dungeon.addEntity(exit);
+        dungeon.addEntity(floorSwitch);
+        dungeon.addEntity(boulder);
+        dungeon.addEntity(treasure);
     }
 
     /**
@@ -59,8 +65,6 @@ public class TestExit {
         Composite and = new And();
         and.add(goalBoulders);
         and.add(goalExit);
-        dungeon.addEntity(boulder);
-        dungeon.addEntity(floorSwitch);
         dungeon.setGoal(and);
         dungeon.setGoal(goalBoulders);
         player.moveDown();
@@ -85,8 +89,6 @@ public class TestExit {
         Composite or = new Or();
         or.add(goalBoulders);
         or.add(goalExit);
-        dungeon.addEntity(boulder);
-        dungeon.addEntity(floorSwitch);
         dungeon.setGoal(or);
         assertFalse(dungeon.isComplete());
         player.moveDown();
@@ -94,9 +96,8 @@ public class TestExit {
     }
 
     @Test
-    public void testComplexGoal() {
+    public void testAndComplexGoal() {
         initialise();
-        Treasure treasure = new Treasure(0, 2);
         Composite and = new And();
         Composite or = new Or();
         and.add(goalTreasure);
@@ -104,13 +105,32 @@ public class TestExit {
         or.add(goalExit);
         or.add(goalBoulders);
         dungeon.setGoal(and);
-        dungeon.addEntity(treasure);
         assertFalse(dungeon.isComplete());
         player.moveDown();
         assertFalse(dungeon.isComplete());
         player.moveBoulder("right");
         assertFalse(dungeon.isComplete());
         player.moveDown(); // Pick up treasure
+        assertTrue(dungeon.isComplete());
+    }
+
+    @Test
+    public void testOrComplexGoal() {
+        initialise();
+        Composite and1 = new And();
+        Composite and2 = new And();
+        Composite or = new Or();
+        or.add(and1);
+        or.add(and2);
+        and1.add(goalExit);
+        and1.add(goalBoulders);
+        and2.add(goalTreasure);
+        and2.add(goalEnemies);
+        dungeon.setGoal(or);
+        assertFalse(dungeon.isComplete());
+        player.moveDown();
+        assertFalse(dungeon.isComplete());
+        player.moveBoulder("right");
         assertTrue(dungeon.isComplete());
     }
 }
