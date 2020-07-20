8ab43b52ec10dabe18a3d1d0b266059cd592e930
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-07-20 16:02:44 +1000

    Test for moveAway

--- src/test/TestMovement.java
+++ src/test/TestMovement.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,24 +9,46 @@ import org.junit.Test;
 import unsw.dungeon.Enemy;
 import unsw.dungeon.Dungeon;
 import unsw.dungeon.Potion;
+import unsw.dungeon.Wall;
 import unsw.dungeon.Player;
 
 public class TestMovement {
     private Dungeon dungeon = new Dungeon(5, 5);
     private Player player = new Player(dungeon, 2, 1);
-    private Enemy enemy = new Enemy(dungeon, 4, 4);
+    private Enemy enemy = new Enemy(dungeon, 1, 0);
+    private Wall wall1 = new Wall(2, 0);
+    private Wall wall2 = new Wall(1, 1);
 
     public void initialise() {
         dungeon.setPlayer(player);
         dungeon.addEntity(player);
         dungeon.addEntity(enemy);
+        dungeon.addEntity(wall1);
+        dungeon.addEntity(wall2);
         player.attach(enemy);
         enemy.initialise(player);
     }
 
-    @Test
-    public void testmoveLeft() {
-        
+    public void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+        }
     }
 
+    private void assertCoordinates(int x, int y) {
+        assertEquals(enemy.getX(), x);
+        assertEquals(enemy.getY(), y);
+    }
+
+    @Test
+    public void testmoveLeft() {
+        initialise();
+        sleep(1050);
+        assertCoordinates(0, 0);
+
+        sleep(500);
+        assertCoordinates(0, 1);
+    }
 }
