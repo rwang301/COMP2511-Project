536691182f18340429c686515fbcba46b2be4633
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-07-28 17:12:55 +1000

    Make move in decorator an abstract method and move the implementation to moveThrough

--- src/unsw/dungeon/Decorator.java
+++ src/unsw/dungeon/Decorator.java
@@ -1,33 +1,11 @@
 package unsw.dungeon;
 
-import java.util.Random;
-
 public abstract class Decorator extends Strategy {
 
-	Decorator(Dungeon dungeon, Gnome gnome) {
-		super(dungeon, gnome);
-	}
+    Decorator(Dungeon dungeon, Gnome gnome) {
+    	super(dungeon, gnome);
+    }
 
-	@Override
-	void move() {
-        if (player.getPrevPosition() != player.getCurrPosition()) { // the gnome only moves when the player moves successfully and not get blocked
-            Random move = new Random();
-            switch (move.nextInt(4)) {
-                case 0:
-                    moveUp();
-                    break;
-                case 1:
-                    moveDown();
-                    break;
-                case 2:
-                    moveLeft();
-                    break;
-                case 3:
-                    moveRight();
-                    break;
-                default:
-                    break;
-            }
-        }
-	}
+    @Override
+    abstract void move();
 }
