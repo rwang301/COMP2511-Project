6a37e0b617e45bf588040c780f7c895df83de260
Verity Webb <z5113759@ad.unsw.edu.au>
2020-08-02 15:17:55 +1000

    added in css for goal scene

--- src/unsw/ui/GoalScene.java
+++ src/unsw/ui/GoalScene.java
@@ -22,10 +22,14 @@ public class GoalScene {
     private double width;
     private double height;
     private Scene scene;
-    private Component goal;
+    private Component goals;
     private Dungeon dungeon;
     private Button back;
     private final double prefDimension;
+    private Label goalExit;
+    private Label goalBoulders;
+    private Label goalTreasure;
+    private Label goalEnemies;
 
     public GoalScene(DungeonApplication application) throws IOException {
         stage = application.getStage();
@@ -35,11 +39,13 @@ public class GoalScene {
 
         dungeon = application.getDungeon();
         goal = dungeon.getGoal();
+        goalDescription();
 
         GridPane gridPane = new GridPane();
         DungeonControllerLoader.loadBackground((int) (width / DungeonControllerLoader.getWidth()), (int) (height / DungeonControllerLoader.getHeight()), gridPane);
-
+        
         StackPane root = new StackPane();
+        root.getStylesheets().add("/styles/goal.css");
         back = ((DungeonApplication) application).backButton();
         root.getChildren().addAll(gridPane, setGoalScene(), back);
         scene = new Scene(root, width, height);
@@ -48,10 +54,23 @@ public class GoalScene {
     private BorderPane setGoalScene() {
         BorderPane borderPane = new BorderPane();
         HBox hBox = new HBox();
-        VBox vBox = new VBox(new Label("Mission"), hBox);
+        for (Component goal : goals.get) {
+
+        }
+        Label label = new Label("Mission");
+        label.setId("mission");
+        VBox vBox = new VBox(label, hBox);
+        System.out.println(goal);
         borderPane.setCenter(vBox);
         vBox.setAlignment(Pos.CENTER);
         return borderPane;
+    }
+
+    public void goalDescription() {
+        goalExit = new Label("GoalExit");
+        goalBoulders = new Label("goalBoulders");
+        goalEnemies = new Label("goalEnemies");
+        goalTreasure= new Label("goalTreasure");
     }
 
     public void start(DungeonController controller) {
