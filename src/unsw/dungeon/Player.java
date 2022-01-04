ccd2f7bf17e36e807bee9cbb62a47998fb19f208
Richard Wang <richardwang301@gmail.com>
2022-01-04 21:32:22 +1100

    Portal class done

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -25,39 +25,60 @@ public class Player extends Entity {
     public void moveUp() {
         if (getY() > 0)
             y().set(getY() - 1);
-        if (isBlocked(Blockable.class)) {
+        if (isOn(Blockable.class)) {
             y().set(getY() + 1);
+        }
+        if (isOn(Portal.class)) {
+            portalTeleport();
         }
     }
 
     public void moveDown() {
         if (getY() < dungeon.getHeight() - 1)
             y().set(getY() + 1);
-        if (isBlocked(Blockable.class)) {
+        if (isOn(Blockable.class)) {
             y().set(getY() - 1);
+        }
+        if (isOn(Portal.class)) {
+            portalTeleport();
         }
     }
 
     public void moveLeft() {
         if (getX() > 0)
             x().set(getX() - 1);
-        if (isBlocked(Blockable.class)) {
+        if (isOn(Blockable.class)) {
             x().set(getX() + 1);
+        }
+        if (isOn(Portal.class)) {
+            portalTeleport();
         }
     }
 
     public void moveRight() {
         if (getX() < dungeon.getWidth() - 1)
             x().set(getX() + 1);
-        if (isBlocked(Blockable.class)) {
+        if (isOn(Blockable.class)) {
             x().set(getX() - 1);
+        }
+        if (isOn(Portal.class)) {
+            portalTeleport();
         }
     }
 
-    private boolean isBlocked(Class<?> blockableType) {
-        System.out.println(getEntityType(blockableType));
-        if (getEntityType(blockableType).contains(this)) return true;
+    private boolean isOn(Class<?> entityType) {
+        if (getEntityType(entityType).contains(this)) return true;
         return false;
+    }
+
+    private void portalTeleport() {
+        Portal matchingPortal = getCurrPortal().getMatchingPortal();
+        x().set(matchingPortal.getX());
+        y().set(matchingPortal.getY());
+    }
+
+    private Portal getCurrPortal() {
+        return (Portal)getEntityType(Portal.class).stream().filter(e -> e.equals(this)).findFirst().get();
     }
 
     private List<Entity> getEntityType(Class<?> entityType) {
