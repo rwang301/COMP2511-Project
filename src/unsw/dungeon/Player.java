c8f91829f967be329f0ae6690def7e478e6d65a9
Richard Wang <richardwang301@gmail.com>
2022-01-07 16:09:08 +1100

    key and door work but not in ui

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -24,39 +24,23 @@ public class Player extends Entity {
         super(x, y);
         this.dungeon = dungeon;
     }
-
     
     public void moveDown() {
         if (getY() < dungeon.getHeight() - 1)
-        y().set(getY() + 1);
-        if (isOn(Blockable.class)) {
-            y().set(getY() - 1);
-        }
-        if (isOn(Portal.class)) {
-            portalTeleport();
-        }
+            y().set(getY() + 1);
+        action(y(), getY() - 1);
     }
     
     public void moveLeft() {
         if (getX() > 0)
-        x().set(getX() - 1);
-        if (isOn(Blockable.class)) {
-            x().set(getX() + 1);
-        }
-        if (isOn(Portal.class)) {
-            portalTeleport();
-        }
+            x().set(getX() - 1);
+        action(x(), getX() + 1);
     }
     
     public void moveRight() {
         if (getX() < dungeon.getWidth() - 1)
-        x().set(getX() + 1);
-        if (isOn(Blockable.class)) {
-            x().set(getX() - 1);
-        }
-        if (isOn(Portal.class)) {
-            portalTeleport();
-        }
+            x().set(getX() + 1);
+        action(x(), getX() - 1);
     }
     
     public void moveUp() {
@@ -67,15 +51,31 @@ public class Player extends Entity {
     private void action(IntegerProperty coordinate, int position) {
         
         if (isOn(Blockable.class)) {
-            coordinate.set(position);
+            if (isOn(Door.class)) {
+                if (!canEnter((Door) getCurrEntity(Door.class))) {
+                    coordinate.set(position);
+                }
+            } else {
+                coordinate.set(position);
+            }
         } else if (isOn(Portal.class)) {
             portalTeleport();
         } else if (isOn(Key.class)) {
-            Key key = (Key)getCurrEntity(Key.class);
+            Key key = (Key) getCurrEntity(Key.class);
             key.pickup(this);
-        } else if (isOn(Door.class)) {
-
         }
+    }
+
+    private boolean canEnter(Door door) {
+        if (!door.getIsOpen()) {
+            if (key == door.getKey()) {
+                door.setIsOpen(true);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 
     public Key getKey() {
@@ -91,26 +91,19 @@ public class Player extends Entity {
     }
 
     private boolean isOn(Class<?> entityType) {
-        System.out.println(getEntityType(entityType));
-        // if (getEntityType(entityType).contains(this))
-        //     return true;
         for (Entity e : getEntityType(entityType)) {
-           if (this.getX() == e.getX() && this.getY() == e.getY()) {
-               return true;
-           }
+            if (this.getX() == e.getX() && this.getY() == e.getY()) {
+                return true;
+            }
         }
         return false;
     }
 
     private void portalTeleport() {
-        Portal matchingPortal = ((Portal)getCurrEntity(Portal.class)).getMatchingPortal();
+        Portal matchingPortal = ((Portal) getCurrEntity(Portal.class)).getMatchingPortal();
         x().set(matchingPortal.getX());
         y().set(matchingPortal.getY());
     }
-
-    // private Portal getCurrPortal() {
-    //     return (Portal) getEntityType(Portal.class).stream().filter(e -> e.equals(this)).findFirst().get();
-    // }
 
     private Entity getCurrEntity(Class<?> entityType) {
         return getEntityType(entityType).stream().filter(e -> e.isOn(this)).findFirst().get();
